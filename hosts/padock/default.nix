cb0630b6096437a0d775ad8c90a6188ebae873b9
ryincler <168774507+ryincler@users.noreply.github.com>
2025-02-24 23:49:27 -0500

    start to move machine-specific configuration to quirks.nix

--- hosts/padock/default.nix
+++ hosts/padock/default.nix
@@ -12,6 +12,7 @@
   optimizeNormalFlags = pkg: optimizeWithFlags pkg ["-O2" "-march=skylake" "-flto" "-pipe"];
 in {
   imports = [
+    ./quirks.nix
     ../../modules/programs/nvf.nix
     ../../modules # TODO: remove usages of ../ imports
     ./hardware-configuration.nix
@@ -207,49 +208,6 @@ in {
 
   services = {
     blueman.enable = true;
-
-    throttled = {
-      enable = true;
-
-      extraConfig = ''
-        [GENERAL]
-        Enabled: True
-        Sysfs_Power_Path: /sys/class/power_supply/AC*/online
-        Autoreload: True
-
-        [BATTERY]
-        Update_Rate_s: 30
-        PL1_Tdp_W: 29
-        PL1_Duration_s: 28
-        PL2_Tdp_W: 44
-        PL2_Duration_S: 0.002
-        Trip_Temp_C: 85
-        cTDP: 0
-        Disable_BDPROCHOT: False
-
-        [AC]
-        Update_Rate_s: 5
-        PL1_Tdp_W: 44
-        PL1_Duration_s: 28
-        PL2_Tdp_W: 44
-        PL2_Duration_S: 0.002
-        Trip_Temp_C: 95
-        # Set HWP energy performance hints to 'performance' on high load (EXPERIMENTAL)
-        # Uncomment only if you really want to use it
-        # HWP_Mode: False
-        # Set cTDP to normal=0, down=1 or up=2 (EXPERIMENTAL)
-        cTDP: 0
-        # Disable BDPROCHOT (EXPERIMENTAL)
-        Disable_BDPROCHOT: False
-
-        [UNDERVOLT]
-        CORE: -105
-        CACHE: -105
-        GPU: -95
-        UNCORE: -90
-        ANALOGIO: 0
-      '';
-    };
 
     openssh = {
       enable = true;
