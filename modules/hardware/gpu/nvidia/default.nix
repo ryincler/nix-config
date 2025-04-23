50beed5bb95e9136605407eaa9371a7cb0c21161
ryincler <168774507+ryincler@users.noreply.github.com>
2025-04-23 01:53:48 -0400

    nvidia: move vaapi driver to nvidia module and add UsePageAttributeTable to kernel params

--- modules/hardware/gpu/nvidia/default.nix
+++ modules/hardware/gpu/nvidia/default.nix
@@ -1,9 +1,10 @@
 {
+  pkgs,
   lib,
   config,
   ...
 }: let
-  inherit (lib) mkEnableOption mkIf mkDefault mkForce;
+  inherit (lib) mkEnableOption mkIf mkDefault mkForce mkMerge;
 in {
   options = {
     nvidia.enable = mkEnableOption "nvidia module";
@@ -15,31 +16,43 @@ in {
       cudaSupport = mkDefault true;
     };
 
-    hardware.nvidia = {
-      powerManagement.enable = mkDefault true;
-      open = mkDefault true;
-      package = mkDefault config.boot.kernelPackages.nvidiaPackages.beta;
-      nvidiaSettings = mkDefault false;
+    hardware = {
+      nvidia = {
+        powerManagement.enable = mkDefault true;
+        open = mkDefault true;
+        package = mkDefault config.boot.kernelPackages.nvidiaPackages.beta;
+        nvidiaSettings = mkDefault false;
+      };
+
+      graphics = {
+        enable = true;
+        enable32Bit = true;
+        extraPackages = with pkgs; [
+          nvidia-vaapi-driver
+        ];
+      };
     };
 
     services.xserver.videoDrivers = ["nvidia"];
 
     boot = {
       kernelParams = [
+        "nvidia.NVreg_UsePageAttributeTable=1"
         "nvidia.NVreg_PreserveVideoMemoryAllocations=1"
       ];
+      /*
       kernelModules = [
         "nvidia"
         "nvidia_modeset"
         "nvidia_drm"
         "nvidia_uvm"
       ];
+      */
     };
 
     nix.settings = {
-      substituters = [ "https://nix-community.cachix.org" ];
-      trusted-public-keys = [ "nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs=" ];
+      substituters = ["https://nix-community.cachix.org"];
+      trusted-public-keys = ["nix-community.cachix.org-1:mB9FSh9qf2dCimDSUo8Zy7bkq5CX+/rkCWyvRCYg3Fs="];
     };
   };
-
 }
