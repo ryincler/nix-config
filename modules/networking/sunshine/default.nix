27920bf81818e88433d4eef994db681711912b1b
ryincler <168774507+ryincler@users.noreply.github.com>
2025-05-13 23:03:33 -0400

    networking/sunshine: change package to follow git

--- modules/networking/sunshine/default.nix
+++ modules/networking/sunshine/default.nix
@@ -6,6 +6,16 @@
 }: let
   inherit (lib) mkOption mkIf;
   cfg = config.modules.networking.sunshine;
+
+  sunshinePkg = pkgs.sunshine.overrideAttrs {
+    src = pkgs.fetchFromGitHub {
+      owner = "LizardByte";
+      repo = "Sunshine";
+      rev = "0a942437e29d8c7938d8f07d2319e634fb295b9b";
+      hash = "sha256-ae0Q0tVkxp+iPWjZL3vCsBjhLEC+oRBVV1CAomFcJtM=";
+      fetchSubmodules = true;
+    };
+  };
 in {
   options = {
     modules.networking.sunshine.enable = mkOption {
@@ -17,6 +27,7 @@ in {
 
   config = mkIf cfg.enable {
     services.sunshine = {
+      package = sunshinePkg;
       enable = true;
       autoStart = true;
       capSysAdmin = true;
