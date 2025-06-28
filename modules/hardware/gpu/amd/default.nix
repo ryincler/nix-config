fc881fdc8a9266bf1769421aa47e12743d00abc1
ryincler <168774507+ryincler@users.noreply.github.com>
2025-06-28 15:44:19 -0400

    modules/hardware/gpu/amd: remove linux-firmware overlay; nixpkgs moved src to a non-problematic commit

--- modules/hardware/gpu/amd/default.nix
+++ modules/hardware/gpu/amd/default.nix
@@ -14,19 +14,6 @@ in {
     };
   };
   config = mkIf cfg.enable {
-    nixpkgs.overlays = [
-      (final: prev: {
-        linux-firmware = prev.linux-firmware.overrideAttrs {
-          version = "20250625-cbbc56e";
-          src = final.fetchFromGitLab {
-            owner = "kernel-firmware";
-            repo = "linux-firmware";
-            rev = "cbbce56d6dcc1ec8fb485dfb92c68cb9acd51410";
-            hash = "sha256-7XN2g4cnHLnICs/ynt8dCpTJbbBkbOdtRm3by/XrDps=";
-          };
-        };
-      })
-    ];
     # To be able to adjust voltage and clock in sysfs
     boot.kernelParams = ["amdgpu.ppfeaturemask=0xffffffff"];
 
