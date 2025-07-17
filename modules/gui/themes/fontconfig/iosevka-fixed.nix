823f9a94e83bfe6ccfbe77c5dc6203faab3e85ec
ryincler <168774507+ryincler@users.noreply.github.com>
2025-07-17 16:32:06 -0400

    iosevka-fixed: 33.2.4 -> 33.2.6

--- modules/gui/themes/fontconfig/iosevka-fixed.nix
+++ modules/gui/themes/fontconfig/iosevka-fixed.nix
@@ -5,11 +5,11 @@
 }:
 stdenv.mkDerivation rec {
   pname = "iosevka-fixed";
-  version = "33.2.4";
+  version = "33.2.6";
 
   src = fetchzip {
     url = "https://github.com/be5invis/Iosevka/releases/download/v${version}/SuperTTC-SGr-IosevkaFixed-${version}.zip";
-    sha256 = "sha256-1bl9nXzzFRXB1BWYl2iTI8qBemzVNHGiJP/79X4q+cw=";
+    sha256 = "sha256-a/YY9BgS7TNX1Gu/jrVOLqvAdMAVXrjKdk7PQ/UXWDw=";
   };
 
   installPhase = ''
