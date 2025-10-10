d59afbe24102227f03e32667e53cab0e9490a64c
ryincler <168774507+ryincler@users.noreply.github.com>
2025-10-10 19:27:29 -0400

    modules/networking/tailscale: init

--- modules/networking/default.nix
+++ modules/networking/default.nix
@@ -2,5 +2,6 @@
   imports = [
     ./zerotierone
     ./sunshine
+    ./tailscale
   ];
 }
---
+++ modules/networking/tailscale/default.nix
@@ -0,0 +1,21 @@
+{
+  config,
+  lib,
+  ...
+}: let
+  cfg = config.modules.networking.tailscale;
+  inherit (lib) mkIf mkEnableOption mkDefault;
+in {
+  options = {
+    modules.networking.tailscale = {
+      enable = mkEnableOption "Tailscale configuration";
+    };
+  };
+  config = mkIf cfg.enable {
+    services.tailscale = {
+      enable = true;
+      permitCertUid = "root";
+      useRoutingFeatures = mkDefault "both";
+    };
+  };
+}
