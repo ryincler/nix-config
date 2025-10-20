20f07eea533f3dc0636199af15ba2d7c968fb959
ryincler <168774507+ryincler@users.noreply.github.com>
2025-10-20 16:20:24 -0400

    hosts/ryou: explicitly define firewall and enable acme nginx tailscale

--- hosts/ryou/default.nix
+++ hosts/ryou/default.nix
@@ -5,10 +5,13 @@
   ];
 
   modules = {
+    security.acme.enable = true;
     services = {
       openssh.enable = true;
+      nginx.enable = true;
       headscale.enable = true;
     };
+    networking.tailscale.enable = true;
   };
 
   nix.settings = {
@@ -27,10 +30,16 @@
     extraGroups = ["wheel"];
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBSxUf9mWU31pwhiPrm6lbX0ap2RJH0sLEU9NuSP8Es3 ry@plaptop"
+      "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIBroVMo8P/KrE3i1P3pw23ehKZlasyFWGpG6naF4N5bl ry@terra"
     ];
   };
   environment.systemPackages = with pkgs; [
     git
     neovim
   ];
+
+  networking.firewall = {
+    enable = true;
+    allowedTCPPorts = [443];
+  };
 }
