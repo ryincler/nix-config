ada7107f17ac5b4f69a1ddf2008ca1902f4f9788
ryincler <168774507+ryincler@users.noreply.github.com>
2025-11-11 14:07:20 -0500

    hosts/plaptop: reduce max temp on AC to match bat max

--- hosts/plaptop/quirks.nix
+++ hosts/plaptop/quirks.nix
@@ -25,7 +25,7 @@
       PL1_Duration_s: 28
       PL2_Tdp_W: 44
       PL2_Duration_S: 0.002
-      Trip_Temp_C: 95
+      Trip_Temp_C: 80
       # Set HWP energy performance hints to 'performance' on high load (EXPERIMENTAL)
       # Uncomment only if you really want to use it
       # HWP_Mode: False
