b3b31184d23d0ba99e9b0011568604c9fbf9f044
ryincler <168774507+ryincler@users.noreply.github.com>
2025-12-03 00:26:32 -0500

    modules/services/tlp: update power configuration on battery

--- modules/services/tlp/default.nix
+++ modules/services/tlp/default.nix
@@ -34,7 +34,7 @@ in {
           CPU_SCALING_GOVERNOR_ON_BAT = "powersave";
 
           CPU_ENERGY_PERF_POLICY_ON_AC = "performance";
-          CPU_ENERGY_PERF_POLICY_ON_BAT = "default";
+          CPU_ENERGY_PERF_POLICY_ON_BAT = "power";
 
           CPU_MIN_PERF_ON_AC = 0;
           CPU_MAX_PERF_ON_AC = 100;
@@ -42,10 +42,13 @@ in {
           CPU_MAX_PERF_ON_BAT = 60;
 
           CPU_BOOST_ON_AC = 1;
-          CPU_BOOST_ON_BAT = 1;
+          CPU_BOOST_ON_BAT = 0;
 
           CPU_HWP_DYN_BOOST_ON_AC = 1;
           CPU_HWP_DYN_BOOST_ON_BAT = 0;
+
+          PLATFORM_PROFILE_ON_AC = "performance";
+          PLATFORM_PROFILE_ON_BAT = "low-power";
         };
       };
     }
