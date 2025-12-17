1fb09fdb8e9e72e45c18ded18974c23c0c8194dd
ryincler <168774507+ryincler@users.noreply.github.com>
2025-12-17 16:27:15 -0500

    modules/hardware/gpu/amd: add nixpkgs overlay for linux-firmware because of a regression in amdgpu

--- modules/hardware/gpu/amd/default.nix
+++ modules/hardware/gpu/amd/default.nix
@@ -1,6 +1,7 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }: let
   inherit (lib) mkOption mkIf;
@@ -14,6 +15,21 @@ in {
     };
   };
   config = mkIf cfg.enable {
+    # Revert amdgpu regression freeze nonsense on linux-firmware version 20251125.
+    # See https://gitlab.freedesktop.org/drm/amd/-/issues/4737
+    nixpkgs.overlays = [
+      (final: prev: {
+        linux-firmware = prev.linux-firmware.overrideAttrs {
+          version = "a0f0e52-git-revert";
+          src = pkgs.fetchFromGitLab {
+            owner = "kernel-firmware";
+            repo = "linux-firmware";
+            rev = "a0f0e52138e5f77fb0f358ff952447623ae0a7c4";
+            hash = "sha256-ukXZjh5OWnFoppD1TVBwHvcXvH4IOoebnQI+pDm/nOk=";
+          };
+        };
+      })
+    ];
     # To be able to adjust voltage and clock in sysfs
     boot.kernelParams = ["amdgpu.ppfeaturemask=0xffffffff"];
 
