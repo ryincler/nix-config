af1f952f73628c0d2cff826bcc9edf2b4613607f
ryincler <168774507+ryincler@users.noreply.github.com>
2025-12-22 21:03:52 -0500

    modules/common: use vim as the included editor instead of yucky nano

--- modules/common/default.nix
+++ modules/common/default.nix
@@ -1,6 +1,12 @@
-{
+{lib, ...}: let
+  inherit (lib) mkDefault;
+in {
   imports = [
     ./nix.nix
   ];
+
+  # Don't include nano as default, use vim instead
+  programs.nano.enable = mkDefault false;
+  programs.vim.enable = mkDefault true;
 }
 
